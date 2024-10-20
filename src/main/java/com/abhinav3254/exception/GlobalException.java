2dda6a534f18e1ea251a2a36659b5053d297abf8
abhinav3254 <abhinav3254@gmail.com>
2024-10-20 21:07:24 +0530

    fixed global exceptions

--- src/main/java/com/abhinav3254/exception/GlobalException.java
+++ src/main/java/com/abhinav3254/exception/GlobalException.java
@@ -1,6 +1,7 @@
 package com.abhinav3254.exception;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,20 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalException {
 
-
     @ExceptionHandler(CustomException.class)
-    private ExceptionWrapper customException(CustomException e) {
-        return new ExceptionWrapper(e.getMessage(),e.getStatus());
+    public ResponseEntity<ExceptionWrapper> handleCustomException(CustomException e) {
+        ExceptionWrapper response = new ExceptionWrapper(e.getMessage(), e.getStatus());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getStatus()));
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
-    private ExceptionWrapper userNotFoundException(UsernameNotFoundException e) {
-        return new ExceptionWrapper(e.getMessage(),404);
+    public ResponseEntity<ExceptionWrapper> handleUserNotFoundException(UsernameNotFoundException e) {
+        ExceptionWrapper response = new ExceptionWrapper(e.getMessage(), 404);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
-    private ExceptionWrapper exception(Exception e) {
-        return new ExceptionWrapper(e.getMessage(),500);
+    public ResponseEntity<ExceptionWrapper> handleGenericException(Exception e) {
+        ExceptionWrapper response = new ExceptionWrapper(e.getMessage(), 500);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
 }
